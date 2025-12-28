59e01ebd0392789e5ceb36077199b8ad39467ea5
Mothmannn <lprupp10@gmail.com>
2025-12-28 13:57:46 -0500

    first commit

---
+++ advanced-ecom-app/src/redux/store.ts
@@ -0,0 +1,9 @@
+// store.ts
+import{ configureStore}from'@reduxjs/toolkit';
+
+export const store=configureStore({
+reducer:{},// Reducers will be added here later
+});
+
+export type RootState= ReturnType<typeof store.getState>;
+export type AppDispatch=typeof store.dispatch;
